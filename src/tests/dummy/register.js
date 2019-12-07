d04557d0fcbd1c102fa3e8c3e90e378066ddf244
Emmanuel <ezeemmanuel2010@gmail.com>
2019-12-07 11:29:34 +0100

    chore(heroku): host app on heroku

--- src/tests/dummy/register.js
+++ src/tests/dummy/register.js
@@ -2,7 +2,7 @@ export default class Register {
   constructor() {
     this.newRegister = {
       name: 'Emmanuel chinazom',
-      phone: 08137370891,
+      phone: '08137370891',
       email: 'emmanuel@gmail.com',
     };
   }
